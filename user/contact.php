ed0f3a875a6fb2bdf34d7ce574606bfc733b397f
BaotrungCode <nguyenbaotrung060@gmail.com>
2022-11-22 21:06:47 +0700

    php_user

--- user/contact.php
+++ user/contact.php
@@ -5,10 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script src="https://kit.fontawesome.com/54f0cb7e4a.js" crossorigin="anonymous"></script>
-	<link rel="stylesheet" href="../css/dangky.css">
+	<link rel="stylesheet" href="../css/contact.css">
 	<link rel="stylesheet" href="./themify-icons/themify-icons.css">
     <link rel="stylesheet" href="./fontawesome-free-6.0.0-web/fontawesome-free-6.0.0-web/css/all.min.css">
-	<title>Đăng ký</title>
+	<title>Liên hệ</title>
 </head>
 <body>
 	<div class="container">
@@ -18,14 +18,14 @@
 		<div class="top-down">
 			<div class="content-top-down">
 				<ul>
-					<li><a href="../user/index.html"><img style="width: 75px; " src="../pictures/icon/logo_1.png" alt=""></a></li>
+					<li><a href="../user/index.php"><img style="width: 75px; " src="../pictures/icon/logo_1.png" alt=""></a></li>
 					<li id="address-form"><a href="#">Hà Nội<i class="fas fa-sort-down"></i></a></li>
 					<li><input type="text" placeholder="Bạn tìm gì..."><i class="fas fa-search"></i></li>
 					<li><a href=""></a><button><i class="fas fa-shopping-cart"></i>Giỏ hàng</button></li>
 					<li><a href="">Lịch sử <br>đơn hàng</a></li>
 					<li><a href="">Mua thẻ nạp ngay!</a></li>
-					<li><a href="../user/hoidap24h.html">24h công nghệ</a></li>
-					<li><a href="../user/hoidap.html">Hỏi đáp</a></li>
+					<li><a href="../user/hoidap24h.php">24h công nghệ</a></li>
+					<li><a href="../user/hoidap.php">Hỏi đáp</a></li>
 					<li><a href="">Game App</a></li>
 				</ul>
 			</div>
@@ -35,7 +35,7 @@
 			<div class="menu-bar-content">
 				<ul class="main-menu">
 					<li>
-						<a href="../user/iphone.html">
+						<a href="../user/iphone.php">
 							<i><img src="../pictures/icon/icon-phone-96x96-2.png"></i>
 							<span>Điện thoại<span>
 						</a>
@@ -77,17 +77,17 @@
 						</a>
 					</li>
 					<li>
-						<a href="../user/contact.html">
+						<a href="../user/contact.php">
 							<span>Contact<span>
 						</a>
 					</li>
 					<li>
-						<a href="../user/login.html" alt="">
+						<a href="../user/login.php" alt="">
 							<span>Đăng nhập<span>
 						</a>
 					</li>
 					<li>
-						<a href="../user/dangky.html" alt="">
+						<a href="../user/dangky.php" alt="">
 							<span>Đăng ký<span>
 						</a>
 					</li>
@@ -95,51 +95,28 @@
 			</div>
 		</div>
 
-
-
-	<div class="main-wrapper">
-		<div class="wrapper">
-			<script language="javascript" src="../user/dangky.js"></script>
-			<form action=""method="POST" onsubmit="return validate()" id="form-login">
-				<h1 class="form-heading">TẠO TÀI KHOẢN</h1>
-				<div class="form-group">
-					<i class="fa-solid fa-user"></i>
-					<input type="text" name="username" id="username" class="form-input" placeholder="Nhập tên đăng nhập">
-				</div>
-				<div class="form-group">
-					<i class="fas fa-key"></i>
-					<input type="password" name="password" id="password" class="form-input" placeholder="Nhập mật khẩu">
-					<div id="eye">
-						<i class="far fa-eye" onclick="daoTT()"></i>
-						<script>
-							function daoTT() {
-								var mk = document.getElementById("password");
-								mk.type = (mk.type === "password")? "text":"password";
-							}  
-							</script>
-					</div>
+	<div class="post-info">
+		<div class="post-info-content">        
+			<p><span><span style="font-size:26px;"><strong>HỆ THỐNG CỬA HÀNG</strong></span><br>
+				<br>
+				<span style="font-size:18px;"><strong>CHI NHÁNH HÀ GIANG:</strong></span><br>
+				<span style="font-size:14px;">Số 215, Đường Trần Phú, TP.Hà Giang, Hà Giang<br>
+				384 Nguyễn Trãi, TP.Hà Giang, Hà Giang<br>
+				Tổ 12, TT Việt Quang, Bắc Quang, Hà Giang</span></span><br>
+				Đường Trần Hưng Đạo, Tổ 5 Nguyễn Trãi, TP. Hà Giang, Hà Giang<br>
+				<span>
+				<span><span style="font-size:18px;">---<br>
+				<strong>CHI NHÁNH BẮC TỪ LIÊM:</strong></span><br>
+				<span style="font-size:14px;">Số 235 Hoàng Quốc Việt - Cổ Nhuế, Bắc Từ Liêm, Hà Nội<br>
+				Tầng 7 - Nhà A, Quận Bắc Từ Liêm<br>
 				
-				</div>
-				<div class="form-group">
-					<i class="fas fa-lock"></i>
-					<input type="password" name="password-repeat" id="password-repeat" class="form-input" placeholder="Nhập lại mật khẩu">
-					<div id="eye">
-						<i class="far fa-eye"onclick="daoTT()"></i>
-						<script>
-							function daoTT() {
-								var mk = document.getElementById("password-repeat");
-								mk.type = (mk.type === "password-repeat")? "text":"password-repeat";
-							}  
-							</script>
-					</div>
-				
-				</div>
-				<div class="form-group">
-					<i class="fa-solid fa-phone"></i>
-					<input type="number" name="number" id="number" class="form-input" placeholder="Nhập số điện thoại">
-				</div>
-				<input type="submit" value="Đăng ký" class="form-submit">
-			</form>
+				---</span><br>
+				<span style="font-size:18px;"><strong>CHI NHÁNH CÀ MAU:</strong></span><br>
+				<span style="font-size:14px;">Số 130 đường Nguyễn Tất Thành, Phường 8, Cà Mau</span><br>
+				<span style="font-size:16px;"></span>Số 394 đường Nguyễn Trãi, Phường 9, Cà Mau (Đối diện đài truyền hình P9)</span>
+			</p>
+			<p><span><span style="font-size:16px;">0988988988</span></span></p>
+			<p><span><span">tmobile</span></span><span><span style="font-size:16px;">@gmail.com</span></span></p>
 		</div>
 	</div>
 	<footer>
@@ -156,9 +133,9 @@
 				  <h3>Quick Links</h3>
 				  <nav>
 					<ul>
-					  <li><a href="../user/index.html">Home</a></li>
-					  <li><a href="../user/iphone.html">Điện thoại</a></li>
-					  <li><a href="../user/hoidap24h.html">Tin tức</a></li>
+					  <li><a href="../user/index.php">Home</a></li>
+					  <li><a href="../user/iphone.php">Điện thoại</a></li>
+					  <li><a href="../user/hoidap24h.php">Tin tức</a></li>
 					</ul>
 				  </nav>
 				</div>
